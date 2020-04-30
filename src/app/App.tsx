6ceb698f6f68cef653eb5306e7d6c74a2e4a0a59
Connor Onweller <onweller@udel.edu>
2020-04-30 17:44:21 -0400

    Added everything

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.scss';
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/nav-bar/nav-bar.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class NavBar extends Component {
+  render(): JSX.Element {
+    return (
+        <nav></nav>
+    );
+  }
+}
