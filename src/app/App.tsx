fc6315a194ab334353ca29109dcedf268c6f7762
Connor Onweller <onweller@udel.edu>
2020-05-02 14:07:10 -0400

    Display logo

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.scss';
-import { NavBar } from '../components';
+import { NavBar } from 'components';
 
 function App(): JSX.Element {
   return (
     <div className="App">
-        <NavBar/>
+      <NavBar/>
     </div>
   );
 }
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
-import './NavBar.scss';
+import './NavBar.scss'
+import logo from 'assets/cssg_logo_black.svg';
 
 function NavLink(props: any): JSX.Element {
-    return (
-    <a className="navlink">
-        {props.name}
-    </a>
-    )
+  let img: JSX.Element | null = null;
+  if (props.img) {
+    img = (
+      <img className="navimage" src={props.img.src} alt={props.img.alt}/>
+    );
+  }
+  return (
+    <button className="navlink">
+      {img}
+      {props.name}
+    </button>
+  )
 }
 
 export class NavBar extends Component {
   render(): JSX.Element {
+    let logo_obj  = {src: logo, alt: "CS + Social Good Logo"};
+
     return (
-        <nav>
-            <NavLink name="hello"/>
-        </nav>
+      <nav>
+        <NavLink img={logo_obj}/>
+      </nav>
     );
   }
 }
