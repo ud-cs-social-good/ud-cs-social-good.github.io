7ef76d6dda478bc44bd3ca510dde5cfe2c0b42b7
Connor Onweller <onweller@udel.edu>
2020-05-02 15:02:38 -0400

    Display navigation links

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './NavBar.scss'
-import logo from 'assets/cssg_logo_black.svg';
+import logo from 'assets/cssg_logo_purple.svg';
 
 function NavLink(props: any): JSX.Element {
   let img: JSX.Element | null = null;
@@ -17,13 +17,30 @@ function NavLink(props: any): JSX.Element {
   )
 }
 
+function NavList(props: any): JSX.Element {
+  let links: [JSX.Element] = (props.links).map((link: any) => (
+    <li><NavLink name={link.name}/></li>
+  ));
+  return (
+    <ul className="navlist">{links}</ul>
+  );
+}
+
 export class NavBar extends Component {
   render(): JSX.Element {
     let logo_obj  = {src: logo, alt: "CS + Social Good Logo"};
-
+    let links = [
+      {name: "Home"},
+      {name: "Events"},
+      {name: "Team"},
+      {name: "Volunteering"},
+    ];
     return (
       <nav>
+        <div className="navcontents">
         <NavLink img={logo_obj}/>
+        <NavList links={links}/>
+        </div>
       </nav>
     );
   }
