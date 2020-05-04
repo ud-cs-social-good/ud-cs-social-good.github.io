c08feabd86a293031161d73dd33fea4b97e66773
Connor Onweller <onweller@udel.edu>
2020-05-04 19:12:57 -0400

    Basic routing working

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,37 +1,41 @@
 import React, { Component } from 'react';
 import './NavBar.scss'
+import { Link } from 'react-router-dom';
 import logo from 'assets/images/cssg_logo_purple.svg';
 
 function NavLink(props: any): JSX.Element {
   return (
-    <button className="navlink">
-      {props.children}
+    <button className='navlink'>
+      <Link to={props.route}>
+        {props.children}
+      </Link>
     </button>
   )
 }
 
 function NavList(props: any): JSX.Element {
   let links: [JSX.Element] = (props.links).map((link: any) => (
-    <li key={link.name}><NavLink>{link.name}</NavLink></li>
+    <li key={link.name}>
+      <NavLink route={link.route}>{link.name}</NavLink>
+    </li>
   ));
   return (
-    <ul className="navlist">{links}</ul>
+    <ul className='navlist'>{links}</ul>
   );
 }
 
 export class NavBar extends Component {
   render(): JSX.Element {
     let links = [
-      {name: "Home"},
-      {name: "Events"},
-      {name: "Team"},
-      {name: "Volunteering"},
+      {name: 'Events', route: '/about'},
+      {name: 'Team', route: '/'},
+      {name: 'Volunteering', route: '/'},
     ];
     return (
       <nav>
-        <div className="navcontents">
-          <NavLink>
-            <img className="navimage" src={logo} alt="CS + Social Good Logo"/>
+        <div className='navcontents'>
+          <NavLink route='/'>
+            <img className='navimage' src={logo} alt='CS + Social Good Logo'/>
           </NavLink>
           <NavList links={links}/>
         </div>
